155ee0ba504534b45d2bde168918a89025e3ff04
skuxy <skuxay@gmail.com>
2022-10-07 15:26:04 +0200

    handling several connections using goroutines

--- app/server.go
+++ app/server.go
@@ -12,18 +12,25 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(2)
-	}
 
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(2)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
 	for {
 		if _, err := conn.Read([]byte{}); err != nil {
 			fmt.Println("Error reading from client: ", err.Error())
 			continue
 		}
-		_, err = conn.Write([]byte("+PONG\r\n"))
+		_, err := conn.Write([]byte("+PONG\r\n"))
 		if err != nil {
 			fmt.Println("Error writing response: ", err.Error())
 			os.Exit(3)
